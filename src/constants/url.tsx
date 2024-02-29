a953e773685b81aa47349d58252f52ea1bf82f51
Nozzlium <Cesa263@gmail.com>
2024-02-29 10:06:13 +0700

    fixed url to https

--- src/constants/url.tsx
+++ src/constants/url.tsx
@@ -1,12 +1,12 @@
-export const BASE_URL = "http://kepo.objectorientedcesa.com/"
+export const BASE_URL = "https://kepo.objectorientedcesa.com/";
 
-export const REGISTER = "register"
-export const LOGIN = "login"
-export const DETAILS = "details"
-export const QUESTION = "question"
-export const ANSWER = "answer"
-export const NOTIFICATION = "notification"
-export const CATEGORIES = "category"
+export const REGISTER = "register";
+export const LOGIN = "login";
+export const DETAILS = "details";
+export const QUESTION = "question";
+export const ANSWER = "answer";
+export const NOTIFICATION = "notification";
+export const CATEGORIES = "category";
 
-export const QUESTION_LIKE = "question/like"
-export const ANSWER_LIKE = "answer/like"
+export const QUESTION_LIKE = "question/like";
+export const ANSWER_LIKE = "answer/like";
